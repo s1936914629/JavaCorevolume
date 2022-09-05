d79e292bbab6a890cd6e9ba60e7ffa922b8500af
s1936914629 <1936914629@qq.com>
2022-09-05 11:45:10 +0800

    第一、二章

---
+++ Chapter1/src/sqx/Welcome/Welcome.java
@@ -0,0 +1,16 @@
+package sqx.Welcome;
+
+/**
+ * @auther: sqx
+ * @Date: 2022-09-05
+ */
+public class Welcome {
+    public static void main(String[] args) {
+        String greeting = "Welcome to Core Java!";
+        System.out.println(greeting);
+        for (int i = 0; i < greeting.length(); i++) {
+            System.out.println("=");
+        }
+        System.out.println();
+    }
+}
