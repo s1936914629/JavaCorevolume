8a406d916a421e7ce5f3cb8ce89554c9b5675fd9
s1936914629 <1936914629@qq.com>
2022-09-06 16:15:47 +0800

    布尔值

---
+++ Chapter3/src/sqx/bool/BooleanDemo.java
@@ -0,0 +1,16 @@
+package sqx.bool;
+
+/**
+ * @auther: sqx
+ * @Date: 2022-09-06
+ */
+public class BooleanDemo {
+    public static void main(String[] args) {
+        boolean b=true;
+        // if (b = 0)
+        //不能将数值型转换为布尔型
+
+        System.out.println(b);
+    }
+
+}
